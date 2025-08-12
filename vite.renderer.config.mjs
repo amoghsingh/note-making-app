a2026830b4ab27cba00debf571dfef45cb1df6a7
Abhijit Sawant <amoghsingh75@gmail.com>
2025-08-12 17:04:23 +0530

    Create vite.renderer.config.mjs

---
+++ vite.renderer.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  root: path.resolve(__dirname, 'src/renderer'),
+  base: './',
+  plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, 'dist/renderer'),
+    emptyOutDir: true
+  }
+});
